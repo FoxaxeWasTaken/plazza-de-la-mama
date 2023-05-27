f2d28cf28ca1ee28294db3a603b3c9162da836c7
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-05-27 18:19:36 +0200

    fix: bad handling of invalid pizza names

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -31,6 +31,9 @@ std::vector<std::unique_ptr<Plazza::IPizza>> Plazza::Parser::checkPizza(std::str
             std::transform(name.cbegin(), name.cend(), name.begin(), [](unsigned char c){return (std::tolower(c));});
             size_t nbPizza = std::stoi(std::string(match[3]).substr(1));
             for (size_t count = 0; count < nbPizza; count++) {
+                if (pizzaFactory.find(name) == pizzaFactory.end()) {
+                    throw (Plazza::ParseError("Invalid pizza name " + std::string(match[1])));
+                }
                 pizzas.emplace_back(std::move(pizzaFactory.find(name)->second(stringToSize.find(std::string(match[2]))->second)));
             }
             ++iter;
